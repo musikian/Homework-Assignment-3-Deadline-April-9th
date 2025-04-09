4d39c4d495ff3286793895d572637f93aee93892
musikian <musikianar@gmail.com>
2025-04-09 17:43:29 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main() {
+    int x, y;
+
+    cout << "Enter the first number: ";
+    cin >> x;
+
+    cout << "Enter the second number: ";
+    cin >> y;
+
+    cout << "\nBefore swap: x = " << x << ", y = " << y << endl;
+
+    swap(x, y);
+
+    cout << "After swap: x = " << x << ", y = " << y << endl;
+
+    return 0;
+}
